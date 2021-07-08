a99c5ec1ced750a4bb0e265ff81fde4cf8bb89df
olabijed@gmail.com <olabijed@gmail.com>
2021-07-08 20:32:01 +0100

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setApplicationName("Snake");
     QCoreApplication::setOrganizationName("Snake");
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
     Snake w;
     w.show();
